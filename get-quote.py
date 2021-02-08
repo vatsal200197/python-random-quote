75d5968161bba3b9898bf5ba8fbcfaa10c09e069
SJCY-24 <sjcy-24@CYS24-KALI01.cyber.int>
2021-02-08 10:17:35 -0400

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def primary():
      print("Keep it logically awesome.")
 
   #if = open("quotes.txt")
@@ -8,4 +8,4 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  primary()
